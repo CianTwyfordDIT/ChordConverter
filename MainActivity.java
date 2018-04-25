4651543ecf80362dc017459eee6cef7d0974a032
CianTwyfordDIT <33058272+CianTwyfordDIT@users.noreply.github.com>
2018-04-25 18:45:09 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.ciant.chordconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button C1,D1,E1,F1,G1,A1,B1,C2,D2,E2,F2,G2,A2,B2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+        C1 = (Button) findViewById(R.id.C1);
+        D1 = (Button) findViewById(R.id.D1);
+        E1 = (Button) findViewById(R.id.E1);
+        F1 = (Button) findViewById(R.id.F1);
+        G1 = (Button) findViewById(R.id.G1);
+        A1 = (Button) findViewById(R.id.A1);
+        B1 = (Button) findViewById(R.id.B1);
+        C2 = (Button) findViewById(R.id.C2);
+        D2 = (Button) findViewById(R.id.D2);
+        E2 = (Button) findViewById(R.id.E2);
+        F2 = (Button) findViewById(R.id.F2);
+        G2 = (Button) findViewById(R.id.G2);
+        A2 = (Button) findViewById(R.id.A2);
+        B2 = (Button) findViewById(R.id.B2);
+
+        C1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        D1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        E1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        F1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        G1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        A1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        C2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        D2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        E2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        F2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        G2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        A2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
